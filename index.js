eb56d442783e852ea3137099358092f852fdf0b5
shakeyiab <shakeyiabartell@gmail.com>
2024-09-23 16:33:31 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+//Registration Form - Username Validation:
+//The username cannot be blank.
+    //<input name="username" type="text" placeholder="Username" required/>
+//The username must be at least four characters long.
+    //minlength="6"
+//The username must contain at least two unique characters.
+//The username cannot contain any special characters or whitespace
